1d0a887b5d5a176aa814bbe7ef59279b163b9509
yashrbankar <yashrbankar@gmail.com>
2022-12-04 20:12:26 +0530

    calcy

---
+++ basic/calculator.py
@@ -0,0 +1,9 @@
+val1 = int(input("val1:: "))
+val2 = int(input("val2:: "))
+
+print("addtion::",val1+val2)
+print("substraction ::",val1-val2)
+print("Division (floor)::",val1//val2)
+print("Multiplication :: ",val1*val2)
+
+
