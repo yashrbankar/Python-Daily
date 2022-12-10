4d10ef5b8da37c396d54132b142213376f0ffd01
yashrbankar <yashrbankar@gmail.com>
2022-12-10 15:20:15 +0530

    streak fucked

---
+++ basic/strings2.py
@@ -0,0 +1,10 @@
+str="yashrbankar"
+print(len(str))
+print(str[3])# h
+print(str[2:7]) #shrba works as substr  starting_index:ending_index(exclusive)
+print(str[-7:-1])# str[len(str)-7:len(str)-1]
+
+print("using loop:")
+
+for char in str:
+    print(char);
